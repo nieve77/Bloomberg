a964c6abb3ba9629d61d743d604ba1f3c2fe87d7
hsyun <nieve1126@gmail.com>
2018-11-29 18:00:05 +0900

    test

---
+++ bloomberg_beta.py
@@ -0,0 +1,12 @@
+import cx_Oracle
+
+con = cx_Oracle.connect('TPO/npi0708@10.0.1.30:1521/PNIDB')
+
+cur=con.cursor()
+
+
+cur.execute("select * from ent_issuinfom where std_dt='20161231'")
+for result in cur:
+    print(result)
+con.close()
+
