9b24083350988e32f75dfeae9d687cb3228b1587
hsyun <nieve1126@gmail.com>
2018-12-05 14:20:25 +0900

    bdp_tot 수정

--- bloomberg_beta.py
+++ bloomberg_beta.py
@@ -9,14 +9,15 @@ class TestPybbg(unittest.TestCase):
         data = tester.bdp(['060310 KS Equity', '095570 KS Equity','068400 KS Equity'], ['BETA_ADJ_OVERRIDABLE', 'BETA_RAW_OVERRIDABLE'])
         print(data)
 
-        data = tester.bdp(['060310 KS Equity', '095570 KS Equity', '068400 KS Equity'],
-                           ['TOT_DEBT_TO_TOT_EQY'],['FUND_PER=Q'])
-        print(data)
-
     def test_bdh(self):
         tester = pybbg.Pybbg()
         data = tester.bdh(['005930 KS Equity'], ['COUNTRY_RISK_RFR','COUNTRY_RISK_PREMIUM'],
-                          datetime.datetime.today() + datetime.timedelta(days=-10), datetime.datetime.today())
+                          datetime.datetime.today() + datetime.timedelta(days=-5), datetime.datetime.today() + datetime.timedelta(days=-1))
+        print(data)
+
+    def test_bdp_tot(self):
+        tester = pybbg.Pybbg()
+        data = tester.bdp(['060310 KS Equity', '095570 KS Equity','068400 KS Equity'],['TOT_DEBT_TO_TOT_EQY'],overrides={'FUND_PER':'Q'})
         print(data)
 
 if __name__ == '__main__':
